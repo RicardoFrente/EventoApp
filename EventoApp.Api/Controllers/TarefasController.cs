5ffba9a7a249f3392e56699f03588e273a89b66f
Ricardo Costa <ricardo@sirrus.com.br>
2022-04-29 21:19:54 -0400

    Entrega

---
+++ EventoApp.Api/Controllers/TarefasController.cs
@@ -0,0 +1,64 @@
+﻿using EventoApp.Core.Data;
+using EventoApp.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventoApp.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        private readonly TarefasContext _db;
+
+        public TarefasController(TarefasContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_db.Tarefas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var tarefa = _db.Tarefas.Find(id);
+            if (tarefa == null)
+                return NotFound();
+            return Ok(tarefa);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ListarTarefas tarefa)
+        {
+            if (tarefa == null)
+                return BadRequest();
+
+            _db.Tarefas.Add(tarefa);
+            _db.SaveChanges();
+            return Created($"tarefas/{tarefa.id}", tarefa);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] ListarTarefas tarefa)
+        {
+            if (id != tarefa.id)
+                return BadRequest();
+            _db.Tarefas.Update(tarefa);
+            _db.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var tarefa = _db.Tarefas.Find(id);
+            if (tarefa == null)
+                return NotFound();
+            _db.Tarefas.Remove(tarefa);
+            _db.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ EventoApp.Api/Migrations/20220428025009_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventoApp.Api.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Tarefa = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Time = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+        }
+    }
+}
